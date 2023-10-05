c968647cf2178de4900add03cc44db88df683771
gastoncaute <gastoncaute@gmail.com>
2023-10-05 19:43:11 -0300

    Se sube proyecto

--- src/app/api/publicaciones/route.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient()
-
-export async function GET() {
-    const publicaciones = await prisma.publicaciones.findMany()
-
-    return new Response(JSON.stringify(publicaciones))
-}
--- src/app/api/usuarios/route.ts
+++ src/app/api/usuarios/route.ts
@@ -1,19 +1,7 @@
-import { PrismaClient } from "@prisma/client"
-
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient()
 
 export async function GET() {
     const usuarios = await prisma.usuarios.findMany()
-
     return new Response(JSON.stringify(usuarios))
-}
-
-export async function POST(request: Request) {
-    const usuarioASubir = await request.json()
-
-    const usuarioSubido = await prisma.usuarios.create({ data: usuarioASubir })
-
-    if(!usuarioASubir) {return new Response("Error! No se pudo subir")}
-
-    return new Response("Usuiario subido exitosamente!")
 }
